49f07f0d2e2e36b4f5423b23795439803c6478e4
fushengss <2259418425@qq.com>
2023-07-13 15:58:50 +0800

    优化页面

--- pages/placeOrder/placeOrder.vue
+++ pages/placeOrder/placeOrder.vue
@@ -1,6 +1,7 @@
 <template>
 	<view>
 		<view class="p-32 bg-white">
+			<map :longitude="longitude" :latitude="latitude" :scale="16" style="width: 100%; height: 250px;" :markers="covers" @click="clickMap"></map>
 			<view class="flex address mt-20" @click="addressPopupShow = true">
 				<image class="icon" src="../../static/location-1.png" mode="aspectFill"></image>
 				<view>请选择地址</view>
@@ -10,9 +11,22 @@
 					<image src="../../static/cae_list_btn_more@2x.png" mode="aspectFill"></image>
 					<view>地址簿</view>
 				</view>
-				<view>{{ userName }}</view>
-				<view>{{ userAreaName }}</view>
-				<view>{{ userAddress }}</view>
+			</view>
+			<view>
+			  <view class="size-30 gray-2 py-28">联系人</view>
+			  <input type="text" class="input-field" v-model="userName" placeholder="请输入姓名">
+			</view>
+			<view>
+			  <view class="size-30 gray-2 py-28">联系号码</view>
+			  <input type="text" class="input-field" v-model="userPhone" placeholder="请输入手机号">
+			</view>
+			<view>
+			  <view class="size-30 gray-2 py-28">社区信息</view>
+			  <input type="text" class="input-field" v-model="userCommunity" placeholder="">
+			</view>
+			<view>
+			  <view class="size-30 gray-2 py-28">详细地址</view>
+			  <input type="text" class="input-field" v-model="userAddress" placeholder="例如:1幢1单元">
 			</view>
 			<view @click="this.isShow=true">
 				<view class="size-30 gray-2 py-28">预约上门时间</view>
@@ -59,7 +73,7 @@
 					</view>
 					<view class="address mt-30 pb-38 border-b">
 						<image class="icon mr-8" src="../../static/map-pin@2x.png" mode="aspectFill"></image>
-						<view class="size-30 gray-9">{{ item.areaName }}{{ item.address }}</view>
+						<view class="size-30 gray-9">{{ item.community }}{{ item.address }}</view>
 					</view>
 				</view>
 			</view>
@@ -89,19 +103,41 @@ export default {
 			userName: '',
 			userPhone: '',
 			userArea: '',
-			userAreaName: '',
+			userLocation: '',
+			userCommunity: '',
 			userAddress: '',
 			dateShow: false,
 			curTime: Number(new Date()),
 			estimateWeight:'',
 			baseUrl: '',
 			userInfo: '',
-			isShow: true,
+			isShow: false,
 			maskHide: true,
 			closeBtn: true,
 			rangeType: true,
 			currentTime:'',
-			currentTimeDate:''
+			currentTimeDate:'',
+			covers: [{
+				latitude: 0,
+				longitude: 0,
+				id: 1,
+				width: 0,
+				height: 0,
+				iconPath: '../../static/location-1.png',
+			}],
+			longitude: 0, // 经度
+			latitude: 0, // 纬度
+			sexList:[
+				{
+					id:0,
+					name:'先生'
+				},
+				{
+					id:1,
+					name:'女士'
+				}
+			],
+			gender:0,
 		};
 	},
 	watch: {
@@ -121,12 +157,19 @@ export default {
 		}
 	},
 	onLoad() {
+		
+
 		this.baseUrl = this.$tools.baseUrl;
-		this.userInfo = uni.getStorageSync('userInfo')
+		this.userInfo = uni.getStorageSync('userInfo');
+		this.getAuthorize();
+		this.initMap();
+		
+		
 	},
 	onShareTimeline() {},
 	onShareAppMessage() {},
 	onShow() {
+		
 		if (!uni.getStorageSync('openid')) {
 			uni.switchTab({
 				url: '../user/user'
@@ -145,8 +188,206 @@ export default {
 					}
 				}
 			});
+		this.resetData();
+		this.initMap();
 	},
 	methods: {
+		
+		resetData() {
+		  this.show = false;
+		  this.addressPopupShow = false;
+		  this.list = '';
+		  this.userId = '';
+		  this.userName = '';
+		  this.userPhone = '';
+		  this.userArea = '';
+		  this.userLocation = '';
+		  this.userCommunity = '';
+		  this.userAddress = '';
+		  this.dateShow = false;
+		  this.curTime = Number(new Date());
+		  this.estimateWeight = '';
+		  this.baseUrl = '';
+		  this.userInfo = '';
+		  this.isShow = false;
+		  this.maskHide = true;
+		  this.closeBtn = true;
+		  this.rangeType = true;
+		  this.currentTime = '';
+		  this.currentTimeDate = '';
+		  this.covers = [{
+		    latitude: 0,
+		    longitude: 0,
+		    id: 1,
+		    width: 0,
+		    height: 0,
+		    iconPath: '../../static/location-1.png',
+		  }];
+		  this.longitude = 0;
+		  this.latitude = 0;
+		  this.sexList = [
+		    {
+		      id: 0,
+		      name: '先生',
+		    },
+		    {
+		      id: 1,
+		      name: '女士',
+		    },
+		  ];
+		  this.gender = 0;
+		},
+		clickMap(p) {
+		  this.longitude = p.detail.longitude;
+		  this.latitude = p.detail.latitude;
+		  this.getSite();
+		  this.covers = [{
+		    latitude: p.detail.latitude,
+		    longitude: p.detail.longitude,
+		    id: 1,
+		    width: 20,
+		    height: 20,
+		    iconPath: '../../static/location-1.png',	
+		  }];
+		  
+		  this.getLocation();
+		},
+
+		getLocation(){
+			const params = {
+			  location: this.latitude+','+this.longitude,
+			  sig: '4NZ8JTPFCfuMz5ND8wewajIo84hlJ4QT',
+			};
+			// 调用定位方法
+			this.qqMap.reverseGeocoder({
+				...params,
+			  success: (res) => {
+			    // 获取定位成功的结果
+			    const community = res.result.formatted_addresses.recommend;
+				this.userCommunity = community;
+			    // 其他逻辑处理
+			    // ...
+			  },
+			  fail: (error) => {
+			    // 定位失败的处理
+			    console.log('定位失败', error)
+			  }
+			})
+		
+		},
+		initMap() {
+		  uni.getLocation({
+		    type: 'gcj02',
+		    success: (res) => {
+		      this.longitude = res.longitude;
+		      this.latitude = res.latitude;
+		      this.covers = [{
+		        latitude: res.latitude,
+		        longitude: res.longitude,
+		        id: 1,
+		        width: 20,
+		        height: 20,
+		        iconPath: '../../static/location-1.png',
+		      }];
+		
+		      // 等待异步回调结果返回后再调用
+		      this.getLocation();
+		      this.getSite();
+		    },
+		    fail: (res) => {
+		      // 处理错误情况
+		    },
+		  });
+		},
+		getAuthorize() {
+		            uni.authorize({
+		              scope: 'scope.userLocation',
+		              success: () => {
+		                this.initMap()
+		              },
+		              fail: (err) => {
+		                err = err['errMsg']
+		                uni
+		                  .showModal({
+		                    content: '需要授权位置信息',
+		                    confirmText: '确认授权'
+		                  })
+						  .then((res) => {
+						        if (res[1]['confirm']) {
+									uni.openSetting({
+						            success: (res) => {
+						            if (res.authSetting['scope.userLocation']) {
+						                // 授权成功
+										uni.showToast({
+						                    title: '授权成功',
+						                    icon: 'none'
+						                })
+						            } else {
+						                // 未授权
+										uni.showToast({
+						                    title: '授权失败',
+						                    icon: 'none'
+						                })
+						            }
+						            this.initMap()
+						        }
+						    })
+						}
+						if (res[1]['cancel']) {
+						// 取消授权
+						console.log('取消')
+						this.initMap()
+						}
+					})
+				}
+			})
+		},
+		getSite() {
+		var ok = 0;
+		this.$api.getAllArea().then(res => {
+		    const locationList = res.data;
+			console.log("data:",locationList)
+		    for (const location of locationList) {
+		      const location1 = location.location.split(',');
+		      const lat1 = parseFloat(location1[0]);
+		      const lon1 = parseFloat(location1[1]);
+		      const lat2 = parseFloat(this.latitude);
+		      const lon2 = parseFloat(this.longitude);
+		      const R = 6371; // 地球半径（单位：公里）
+		
+		      const dLat = toRad(lat2 - lat1);
+		      const dLon = toRad(lon2 - lon1);
+			  console.log(dLat);
+			  console.log(dLon);
+		
+		      const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+		        Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) *
+		        Math.sin(dLon / 2) * Math.sin(dLon / 2);
+		
+		      const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+		      const distance = R * c;
+			  console.log(distance)
+		
+		      if (distance <= 5 && distance >= -5) {
+		        this.userArea = location.id;
+				ok=1;
+		        break;
+		      }else{
+				  ok = 2;
+			  }
+		    }
+			if (ok==2) {
+			  uni.showToast({
+			    title: '当前位置未开通服务',
+			    icon: 'none'
+			  });
+			}
+		  });
+		
+		  function toRad(value) {
+		    return value * Math.PI / 180;
+		  }
+		},
 		showMask() {
 		    this.isShow = true;
 		    console.log(this.isShow);
@@ -166,7 +407,6 @@ export default {
 				})
 				.then(res => {
 					this.list = res.data;
-					console.log(res);
 				});
 		},
 		addAddress() {
@@ -178,13 +418,24 @@ export default {
 		selectAddress(item) {
 			this.userName = item.userName;
 			this.userPhone = item.userPhone;
-			this.userAreaName = item.areaName;
+			this.userCommunity = item.community;
 			this.userAddress = item.address;
 			this.userArea = item.areaId;
+			const location1 = item.location.split(',');
+			this.latitude = location1[0];
+			this.longitude = location1[1];
 			this.addressPopupShow = false;
+			this.covers = [{
+				latitude: this.latitude,
+				longitude: this.longitude,
+				id: 1,
+				width: 20,
+				height: 20,
+				iconPath: '../../static/location-1.png',	
+			}]
+			this.getSite();
 		},
 		submit(type) {
-			console.log(this.areaId);
 			if(this.userName && this.estimateWeight && this.currentTime){
 				this.$api
 				.placeOrder({
@@ -192,7 +443,8 @@ export default {
 					userName: this.userName,
 					userPhone: this.userPhone,
 					userArea: this.userArea,
-					userAreaName: this.userAreaName,
+					userLocation: this.latitude+','+this.longitude,
+					userCommunity: this.userCommunity,
 					userAddress: this.userAddress,
 					reserveTime: this.currentTimeDate,
 					estimateWeight:this.estimateWeight
@@ -209,10 +461,10 @@ export default {
 										page.onLoad();
 									}
 								});
-							}, 1000);
+							}, 10);
 						}
 					else {
-						this.$tools.toast(res.msg);
+						this.$tools.toast('当前所在地区未在服务范围内');
 					}
 				});
 				}else{
@@ -265,6 +517,54 @@ export default {
 .border-b {
 	border-bottom: 1upx solid #efefef;
 }
+.input {
+			margin: 0 20upx;
+			padding: 0 20upx;
+			line-height: 100upx;
+			display: flex;
+			align-items: center;
+			font-size: 30upx;
+			color: #999999;
+			border-bottom: 2upx solid #ededed;
+			background: #fff;
+			position: relative;
+			.sex{
+				display: flex;
+				margin: 20upx 0;
+				.item{
+					font-size: 32upx;
+					color: #C4C4C4;
+					width: 128upx;
+					line-height: 72upx;
+					text-align: center;
+					background: #FFFFFF;
+					border-radius: 12upx;
+					border: 1upx solid #C4C4C4;
+					margin-right: 32upx;
+				}
+				.cur{
+					color: #3BB061;
+					border: 1upx solid #3BB061;
+				}
+			}
+			text {
+				font-size: 30upx;
+				width: 170upx;
+				min-width: 170upx;
+				color: #222;
+			}
+			
+			.text-1{
+				padding-right: 50upx;
+				line-height: 1.6;
+				margin: 10upx 0;
+			}
+
+			input {
+				font-size: 28upx;
+				color: #999999;
+			}
+		}
 
 .type-list {
 	white-space: nowrap;
