c337d19f7f2c1281b396bbdc03e4959a1bee7208
username <jflachenjiahao@163.com>
2023-07-24 14:18:30 +0800

    修改了下单页面

--- pages/placeOrder/placeOrder.vue
+++ pages/placeOrder/placeOrder.vue
@@ -2,43 +2,83 @@
 	<view class="container">
 		<map :longitude="longitude" :latitude="latitude" :scale="16" style="width: 100%; height: 100%; position: absolute;" :markers="covers" @click="clickMap"></map>
 		<view class="contain">
-			<view class="search">
-				<span class="iconfont" @click="searchAddress">&#xeafe;</span>
+<!-- 			<view class="search">
+				<span class="iconfont" @click="searchAddress(searchKeyword)">&#xeafe;</span>
 				<input type="text" v-model="searchKeyword" placeholder="浙江省湖州市">
-			</view>
+			</view> -->
 			<view class="info">
 				<view class="info-contain">
 					<view class="info-header">
-						<span style="font-weight: 600;">废品回收</span>
-						<span class="iconfont" style="color: green;">&#xe628;1.2km</span>
+						<span style="font-weight: 600;">{{userName}}</span>
+						<!-- <span class="iconfont" style="color: green;">&#xe628;1.2km</span> -->
+					</view>
+					<view class="info-community">
+						<span class="iconfont">&#xe652;</span>
+						<!-- <span style="margin-left: 1upx;">浙江省湖州市</span> -->
+						<input type="text" style="margin-left: 1upx; flex-grow: 1;" placeholder="浙江省湖州市" v-model="userCommunity" @blur="searchAddress(userCommunity)">
 					</view>
 					<view class="info-address">
-						<span class="iconfont">&#xe652;</span>
-						<span style="margin-left: 1upx;">浙江省湖州市</span>
+						<span class="iconfont">&#xe624;</span>
+						<input type="text" style="margin-left: 1upx; flex-grow: 1;" placeholder="请输入详细地址" v-model="userAddress">
 					</view>
 					<view class="info-phone">
 						<span class="iconfont">&#xe88b;</span>
-						<span style="margin-left: 1upx;">123456</span>
+						<input type="text" style="margin-left: 1upx; flex-grow: 1;" placeholder="请输入手机号" v-model="userPhone">
 					</view>
-					<view class="info-date">
+					<view class="info-date" @click="this.isShow=true">
 						<span class="iconfont">&#xe64d;</span>
-						<span style="margin-left: 1upx;">8:00 - 11:00</span>
+						<!-- <u-input v-model="currentTime" placeholder="请选择" disabled disabledColor="#ffffff" style="margin-left: 1upx; padding: 0;"></u-input>				 -->
+						<input type="text" style="margin-left: 1upx; flex-grow: 1;" :placeholder="isShow ? '' : '请选择时间'" v-model="currentTime">
+						<hTimeAlert title="预约时间" subhead='请选择需要上门服务的时间' rangeDay="5" intervalTime="60" :isShow="isShow" @closeAlert="handelClose" class="hTimeAlert"></hTimeAlert>
+					</view>
+					<view class="info-category">
+						<span class="iconfont">&#xe62a;</span>
+						<input type="text" style="margin-left: 1upx; flex-grow: 1;" placeholder="请输入旧物种类" v-model="recycleCategory">
 					</view>
 				</view>
 
 				<view class="info-footer">
-					<view class="info-footer-left">
+					<view class="info-footer-left" @click="addressPopupShow = true">
 						<span class="iconfont">&#xe74c;</span>
-						<span style="margin-left: 1upx;">定位</span>
+						<span style="margin-left: 1upx;">地址簿</span>
 					</view>
-					<view class="info-footer-right">
+					<view class="info-footer-middle">
 						<span class="iconfont">&#xe625;</span>
 						<span style="margin-left: 1upx;" @click="goBooking">预约</span>
 					</view>
-					
+					<view class="info-footer-right" @click="submit(2)">
+						<span class="iconfont">&#xec36;</span>
+						<span style="margin-left: 1upx;">确认</span>
+					</view>
 				</view>
 			</view>
 		</view>
+		<!-- 选择地址 -->
+		<u-popup :show="addressPopupShow" mode="bottom" @close="addressPopupShow = false">
+			<view class="empty" v-if="list.length == 0">
+				<image src="../../static/empty.png" mode="aspectFill"></image>
+				<view>添加地址，预约你的回收天使～</view>
+			</view>
+			<view class="card">
+				<view class="item" v-for="item in list" :key="item.id" @click="selectAddress(item)">
+					<view class="flex-between">
+						<view class="size-30">
+							{{ item.userName }}
+							<text class="ml-30 gray-2">{{ item.userPhone }}</text>
+						</view>
+						<!-- <image class="icon" src="../../static/edit@2x.png" mode=""></image> -->
+					</view>
+					<view class="address mt-30 pb-38 border-b">
+						<image class="icon mr-8" src="../../static/map-pin@2x.png" mode="aspectFill"></image>
+						<view class="size-30 gray-9">{{ item.community }}{{ item.address }}</view>
+					</view>
+				</view>
+			</view>
+			<view class="btn-3 btn" @click="addAddress">
+				<image src="../../static/address_btn_add_new@2x.png" mode="aspectFill"></image>
+				添加地址
+			</view>
+		</u-popup>
 		<orderStatus :show="show" @closePopup="closePopup"></orderStatus>
 	</view>
 </template>
@@ -47,6 +87,10 @@
 import orderStatus from '../../components/onTakeOrder/onTakeOrder.vue';
 import hTimeAlert from '../../components/h-time-alert/h-time-alert.vue';
 export default {
+	components: {
+		orderStatus,
+		hTimeAlert
+	},
 	data() {
 		return {
 			show: false,
@@ -62,6 +106,7 @@ export default {
 			dateShow: false,
 			curTime: Number(new Date()),
 			estimateWeight:'',
+			recycleCategory: '',
 			baseUrl: '',
 			userInfo: '',
 			isShow: false,
@@ -107,7 +152,7 @@ export default {
 			m = m < 10 ? '0' + m : m;
 			d = d < 10 ? '0' + d : d;
 			this.reserveTime = `${y}/${month}/${d} ${h}:${m}`;
-			console.log(this.time);
+			// console.log(this.time);
 		}
 	},
 	onLoad() {
@@ -122,10 +167,6 @@ export default {
 		}
 
 		this.baseUrl = this.$tools.baseUrl;
-		this.userInfo = uni.getStorageSync('userInfo');
-
-		
-		
 	},
 	onShareTimeline() {},
 	onShareAppMessage() {},
@@ -151,6 +192,8 @@ export default {
 			});
 		this.resetData();
 		this.initMap();
+		// 在onload中uni.getStorageSync('userInfo').name可能还未设置完毕
+		this.userName = uni.getStorageSync('userInfo').name || '微信用户'
 	},
 	methods: {
 		
@@ -261,104 +304,104 @@ export default {
 		  });
 		},
 		getAuthorize() {
-		            uni.authorize({
-		              scope: 'scope.userLocation',
-		              success: () => {
-		                this.initMap()
-		              },
-		              fail: (err) => {
-		                err = err['errMsg']
-		                uni
-		                  .showModal({
-		                    content: '需要授权位置信息',
-		                    confirmText: '确认授权'
-		                  })
-						  .then((res) => {
-						        if (res[1]['confirm']) {
-									uni.openSetting({
-						            success: (res) => {
-						            if (res.authSetting['scope.userLocation']) {
-						                // 授权成功
+			uni.authorize({
+				  scope: 'scope.userLocation',
+				  success: () => {
+					this.initMap()
+				  },
+				  fail: (err) => {
+					err = err['errMsg']
+					uni
+					  .showModal({
+						content: '需要授权位置信息',
+						confirmText: '确认授权'
+					  })
+					  .then((res) => {
+						if (res[1]['confirm']) {
+							uni.openSetting({
+								success: (res) => {
+									if (res.authSetting['scope.userLocation']) {
+										// 授权成功
 										uni.showToast({
-						                    title: '授权成功',
-						                    icon: 'none'
-						                })
-						            } else {
-						                // 未授权
+											title: '授权成功',
+											icon: 'none'
+										})
+									} else {
+										// 未授权
 										uni.showToast({
-						                    title: '授权失败',
-						                    icon: 'none'
-						                })
-						            }
-						            this.initMap()
-						        }
-						    })
+											title: '授权失败',
+											icon: 'none'
+										})
+									}
+									this.initMap()
+								}
+							})
 						}
 						if (res[1]['cancel']) {
-						// 取消授权
-						console.log('取消')
-						this.initMap()
+							// 取消授权
+							console.log('取消')
+							this.initMap()
 						}
 					})
 				}
 			})
 		},
 		getSite() {
-		var ok = 0;
-		this.$api.getAllArea().then(res => {
-		    const locationList = res.data;
-			console.log("data:",locationList)
-		    for (const location of locationList) {
-		      const location1 = location.location.split(',');
-		      const lat1 = parseFloat(location1[0]);
-		      const lon1 = parseFloat(location1[1]);
-		      const lat2 = parseFloat(this.latitude);
-		      const lon2 = parseFloat(this.longitude);
-		      const R = 6371; // 地球半径（单位：公里）
-		
-		      const dLat = toRad(lat2 - lat1);
-		      const dLon = toRad(lon2 - lon1);
-			  console.log(dLat);
-			  console.log(dLon);
-		
-		      const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-		        Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) *
-		        Math.sin(dLon / 2) * Math.sin(dLon / 2);
-		
-		      const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-		      const distance = R * c;
-			  console.log(distance)
-		
-		      if (distance <= 5 && distance >= -5) {
-		        this.userArea = location.id;
-				ok=1;
-		        break;
-		      }else{
-				  ok = 2;
-			  }
-		    }
-			if (ok==2) {
-			  uni.showToast({
-			    title: '当前位置未开通服务',
-			    icon: 'none'
+			var ok = 0;
+			this.$api.getAllArea().then(res => {
+				const locationList = res.data;
+				// console.log("data:",locationList)
+				for (const location of locationList) {
+				  const location1 = location.location.split(',');
+				  const lat1 = parseFloat(location1[0]);
+				  const lon1 = parseFloat(location1[1]);
+				  const lat2 = parseFloat(this.latitude);
+				  const lon2 = parseFloat(this.longitude);
+				  const R = 6371; // 地球半径（单位：公里）
+			
+				  const dLat = toRad(lat2 - lat1);
+				  const dLon = toRad(lon2 - lon1);
+				  // console.log(dLat);
+				  // console.log(dLon);
+			
+				  const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+					Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) *
+					Math.sin(dLon / 2) * Math.sin(dLon / 2);
+			
+				  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+				  const distance = R * c;
+				  // console.log(distance)
+			
+				  if (distance <= 5 && distance >= -5) {
+					this.userArea = location.id;
+					ok=1;
+					break;
+				  }else{
+					  ok = 2;
+				  }
+				}
+				if (ok==2) {
+				  uni.showToast({
+					title: '当前位置未开通服务',
+					icon: 'none'
+				  });
+				}
 			  });
-			}
-		  });
-		
-		  function toRad(value) {
-		    return value * Math.PI / 180;
-		  }
+			
+			  function toRad(value) {
+				return value * Math.PI / 180;
+			  }
 		},
 		showMask() {
 		    this.isShow = true;
-		    console.log(this.isShow);
+		    // console.log(this.isShow);
 		},
 		
 		handelClose(data) {
 		    this.isShow = false;
 			this.currentTime = data.dateRange;
 			this.currentTimeDate = data.timeStamp;
-		    console.log(data); 
+		    // console.log(data); 
 		},
 		// 获取地址列表
 		getList() {
@@ -397,7 +440,7 @@ export default {
 			this.getSite();
 		},
 		submit(type) {
-			if(this.userName && this.estimateWeight && this.currentTime){
+			if(this.userName && this.recycleCategory && this.currentTime){
 				this.$api
 				.placeOrder({
 					userId: uni.getStorageSync('openid'),
@@ -408,10 +451,10 @@ export default {
 					userCommunity: this.userCommunity,
 					userAddress: this.userAddress,
 					reserveTime: this.currentTimeDate,
-					estimateWeight:this.estimateWeight
+					recycleCategory:this.recycleCategory
 				})
 				.then(res => {
-					console.log(res.code)
+					console.log(this.latitude,'***',this.longitude)
 					if (res.code == 200) {
 						this.$tools.toast('下单成功');
 							setTimeout(() => {
@@ -441,12 +484,12 @@ export default {
 				url: '../booking/booking'
 			})
 		},
-		searchAddress() {
+		searchAddress(keyword) {
 		    this.qqMap.geocoder({
-		        address: '浙江省湖州市'+this.searchKeyword, //地址参数，例：固定地址，address: '北京市海淀区彩和坊路海淀西大街74号'
+		        address: '浙江省湖州市'+keyword, //地址参数，例：固定地址，address: '北京市海淀区彩和坊路海淀西大街74号'
 				sig:'4NZ8JTPFCfuMz5ND8wewajIo84hlJ4QT',
 		              success: (res) => {//成功后的回调
-		                console.log(res);
+		                // console.log(res);
 		                this.latitude = res.result.location.lat;
 		                this.longitude = res.result.location.lng;
 						this.community = res.result.title;
@@ -475,9 +518,9 @@ export default {
 <style lang="scss">
 	@font-face {
 	  font-family: 'iconfont';  /* Project id 4143017 */
-	  src: url('//at.alicdn.com/t/c/font_4143017_f018g5hwvye.woff2?t=1689766998043') format('woff2'),
-		   url('//at.alicdn.com/t/c/font_4143017_f018g5hwvye.woff?t=1689766998043') format('woff'),
-		   url('//at.alicdn.com/t/c/font_4143017_f018g5hwvye.ttf?t=1689766998043') format('truetype');
+	  src: url('//at.alicdn.com/t/c/font_4143017_ulbzv7r9ka.woff2?t=1690008329887') format('woff2'),
+		   url('//at.alicdn.com/t/c/font_4143017_ulbzv7r9ka.woff?t=1690008329887') format('woff'),
+		   url('//at.alicdn.com/t/c/font_4143017_ulbzv7r9ka.ttf?t=1690008329887') format('truetype');
 	}
 	.iconfont {
 	  font-family: "iconfont" !important;
@@ -485,6 +528,48 @@ export default {
 	  font-style: normal;
 	  -webkit-font-smoothing: antialiased;
 	  -moz-osx-font-smoothing: grayscale;
+	}
+	.empty {
+		text-align: center;
+		margin: 100upx auto 120upx;
+	}
+	.card {
+		.item {
+			background: #fff;
+			padding: 28upx;
+			margin-bottom: 16upx;
+			border-radius: 12upx;
+	
+			.address {
+				display: flex;
+	
+				.icon {
+					margin-top: 4upx;
+				}
+			}
+	
+			.icon {
+				min-width: 32upx;
+				width: 32upx;
+				height: 32upx;
+			}
+	
+			.radio {
+				transform: scale(0.9);
+			}
+		}
+	}
+	.btn-3 {
+		width: 700upx;
+		margin: 0 auto 30upx;
+	}
+	.btn {
+		width: 320upx;
+		border-radius: 12upx;
+		font-size: 32upx;
+		font-weight: 400;
+		color: #ffffff;
+		line-height: 88upx;
 	}
 	.container {
 		position: absolute;
@@ -524,27 +609,49 @@ export default {
 				// bottom: 100upx;
 				bottom: 10%;
 				// height: 300upx;
-				height: 50%;
+				height: 288px;
 				width: 100%;
 				border-radius: 20upx;
 				background-color: #fff;
 				box-sizing: border-box;
 				overflow: hidden;
-				font-size: 26upx;
+				font-size: 24upx;
 				.info-contain {
 					padding: 20upx;
 					.info-header {
 						display: flex;
 						justify-content: space-between;
-						margin-bottom: 10upx;
+						margin-bottom: 17px;
+						font-size: 35upx;
 					}
-					.info-address {
+					.info-community {
+						display: flex;
+						align-items: center;
+						margin-bottom: 17px;
 						color: #898989
 					}
 					.info-phone {
+						display: flex;
+						align-items: center;
+						margin-bottom: 17px;
 						color: #898989
 					}
 					.info-date {
+						display: flex;
+						align-items: center;
+						margin-bottom: 17px;
+						color: #898989
+					}
+					.info-category {
+						display: flex;
+						align-items: center;
+						margin-bottom: 17px;
+						color: #898989
+					}
+					.info-address {
+						display: flex;
+						align-items: center;
+						margin-bottom: 17px;
 						color: #898989
 					}
 				}
@@ -568,10 +675,14 @@ export default {
 						flex: 1;
 						color: #fff;
 					}
+					.info-footer-middle {
+						flex: 1;
+						border-right: 1upx #fff dashed;
+						color: #fff;
+					}
 				}
 			}
 		}
-
 	}
 
 
