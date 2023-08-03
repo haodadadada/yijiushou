42f89d301da784b5854fb211e218627f5163c80d
xiazhihao <2533872859@qq.com>
2023-08-03 14:05:20 +0800

    首页/我的alipay端样式

--- pages/user/user.vue
+++ pages/user/user.vue
@@ -1,11 +1,12 @@
 <template>
 	<view class="container">
 		<u-navbar title="个人中心" leftIconSize="0" :bgColor="bgColor"></u-navbar>
-		<view class="bgc1">
+		<view :class="showInalipay? 'bgcali1': 'bgc1'">
 		</view>
-		<view class="bgc2">
+		<view :class="showInalipay? 'bgcali2': 'bgc2'">
 		</view>
-		<view class="header">
+		<view class="info-container">
+			<view class="header">
 			<view class="userInfo" @click="editUser" v-if="openid">
 				<!-- <img src="../../static/new-user2.png" alt="指南" class="user-img"> -->
 				<img :src="avatarUrl" alt="头像" class="user-img">
@@ -81,6 +82,8 @@
 				</view>
 			</view>
 		</view>
+		</view>
+			
 		<orderStatus :show="show" @closePopup="closePopup"></orderStatus>
 	</view>
 </template>
@@ -156,7 +159,9 @@ export default {
 			],
 			bannerList: [],
 			user_id: '',
-			avatarUrl: 'https://pic4.zhimg.com/50/v2-6afa72220d29f045c15217aa6b275808_hd.jpg?source=1940ef5c'
+			avatarUrl: 'https://pic4.zhimg.com/50/v2-6afa72220d29f045c15217aa6b275808_hd.jpg?source=1940ef5c',
+			systemInfo: '',
+			showInalipay: false
 		};
 	},
 	onShareAppMessage() {},
@@ -167,6 +172,7 @@ export default {
 			this.user_id = e.scene;
 			uni.setStorageSync('invition_id', this.user_id);
 		}
+		this.getSystemInfo()
 	},
 	onShow() {
 		this.getUserInfo();
@@ -312,6 +318,13 @@ export default {
 				}
 			});
 		},
+		/** 获取设备 */ 
+		getSystemInfo() {
+			this.systemInfo = uni.getSystemInfoSync()
+			if(this.systemInfo.uniPlatform === 'mp-alipay') {
+				this.showInalipay = true
+			}
+		}
 	}
 };
 </script>
@@ -333,6 +346,14 @@ body {
 	background: linear-gradient(45deg, #70c9ae, #77c9b0);
 	z-index: -1;
 }
+.bgcali1{
+	position: absolute;
+	top: 0;
+	left: 0;
+	right: 0;
+	height: 415upx;
+	background-image: url('../../static/bg-user-alipay-1.jpg');
+}
 
 .bgc2 {
 	position: absolute;
@@ -342,6 +363,14 @@ body {
 	height: 850upx;
 	background: linear-gradient(to top, #9fe6ce, #fff);
 	z-index: -2;
+}
+.bgcali2 {
+	position: absolute;
+	bottom: 0;
+	left: 0;
+	right: 0;
+	height: 850upx;
+	background-image: url('../../static/bg-user-alipay-2.jpg');
 }
 
 .header{
@@ -562,5 +591,8 @@ body {
 			margin-bottom: 10upx;
 		}
 	}
+}
+.info-container{
+	position: relative;
 }
 </style>
